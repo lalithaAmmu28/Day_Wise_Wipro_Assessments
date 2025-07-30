770f988b76e5735c61d70faa5ec31626f876703a
Gudala Lalitha Maheswari <111893584+lalithaAmmu28@users.noreply.github.com>
2025-07-30 16:21:09 +0530

    Add files via upload

---
+++ Day10_Productdemo/src/test/java/com/example/productdemo/Productdemo/ProductdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productdemo.Productdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
