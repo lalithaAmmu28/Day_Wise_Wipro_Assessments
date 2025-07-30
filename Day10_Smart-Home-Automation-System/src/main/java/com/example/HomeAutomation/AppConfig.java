ee7ac61a0c4df5bd074ecd69bac1f0c18b6df756
Gudala Lalitha Maheswari <111893584+lalithaAmmu28@users.noreply.github.com>
2025-07-30 23:42:04 +0530

    Delete Day10_Smart-Home-Automation-System directory

--- Day10_Smart-Home-Automation-System/src/main/java/com/example/HomeAutomation/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.HomeAutomation;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.example.HomeAutomation") 
-public class AppConfig {
-}
